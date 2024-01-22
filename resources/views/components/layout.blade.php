e2ae0db400364139cdb32dcee127eb6962958a1a
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-01-22 15:05:43 -0300

    :lipstick: style: tailwind css instalado

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,51 +6,52 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - Controle de Séries</title>
+
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/lista-series.css') }}">
-    <script src="https://kit.fontawesome.com/your-kit-id.js"></script>
+    {{-- <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/lista-series.css') }}"> --}}
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('series.index') }}">Home</a>
-
-        @auth
-        <form action="{{ route('logout') }}" method="post">
-            @csrf
-            <button class="btn btn-link">
-                Sair
-            </button>
-        </form>
-        @endauth
-
-        @guest
-        <a href="{{ route('login') }}">Entrar</a>
-        @endguest
+<body class="font-sans antialiased bg-gray-100 min-h-screen">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <div>
+            <a href="{{ route('series.index') }}" class="text-lg font-bold text-gray-800">Home</a>
+        </div>
+    
+        <div>
+            @auth
+            <form action="{{ route('logout') }}" method="post" class="p-3">
+                @csrf
+                <button class="text-blue-500">Sair</button>
+            </form>
+            @endauth
+    
+            @guest
+            <a href="{{ route('login') }}" class="p-3 text-blue-500">Entrar</a>
+            @endguest
+        </div>
+    </nav>
+    <div class="flex justify-center">
+        <div class="w-full sm:w-1/2 lg:w-1/3 bg-white p-6 rounded-lg">
+            <h1 class="text-2xl font-medium mb-1">{{ $title }}</h1>
+    
+            @isset($mensagemSucesso)
+                <div class="bg-green-500 text-white px-4 py-2 rounded font-medium w-full">
+                    {{ $mensagemSucesso }}
+                </div>
+            @endisset
+    
+            @if ($errors->any())
+                <div class="bg-red-500 text-white px-4 py-2 rounded font-medium w-full">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+    
+            {{ $slot }}
+        </div>
     </div>
-</nav>
-<div class="container">
-    <h1>{{ $title }}</h1>
-
-    @isset($mensagemSucesso)
-        <div class="alert alert-success">
-            {{ $mensagemSucesso }}
-        </div>
-    @endisset
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    {{ $slot }}
-</div>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-    .sass('resources/css/app.scss', 'public/css');
+mix.js('resources/css/app.scss', 'public/css')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss')
+]);
+
